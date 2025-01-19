c7ffaaa35c28e82c9fd8d2f56303b67b35f8eed6
kyramirez <kramir62@calpoly.edu>
2025-01-18 20:03:54 -0800

    commiting the mac part

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 const port = 8000;
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
 
 app.use(express.json());
 
@@ -44,5 +47,12 @@ const users = {
 };
 
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
