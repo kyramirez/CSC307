78d3369e19e1daeb7cc7c528a9f15a54264320c0
kyramirez <kramir62@calpoly.edu>
2025-01-21 17:51:22 -0800

    final

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -2,9 +2,6 @@ import express from "express";
 
 const app = express();
 const port = 8000;
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
 
 app.use(express.json());
 
@@ -43,16 +40,78 @@ const users = {
       name: "Dennis",
       job: "Bartender",
     },
+    {
+      id: "qwe123",
+      job: "Zookeeper",
+      name: "Cindy",
+    },
   ],
 };
 
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+// app.get("/users", (req, res) => {
+//   const name = req.query.name;
+//   if (name != undefined) {
+//     let result = findUserByName(name);
+//     result = { users_list: result };
+//     res.send(result);
+//   } else {
+//     res.send(users);
+//   }
+// });
+
 app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+  const { name, job } = req.query;
+  let result = users.users_list;
+  if (name) {
+    result = result.filter((user) => user.name === name);
+  }
+  if (job) {
+    result = result.filter((user) => user.job === job);
+  }
+  if (result.length > 0) {
+    res.send({ users_list: result });
   } else {
-    res.send(users);
+    res.status(404).send("No users found.");
+  }
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const original = users.users_list.length;
+  const filteredLst = users.users_list.filter((user) => user.id !== id);
+
+  if (filteredLst.length === original) {
+    res.status(404).send("User not found.");
+  } else {
+    users.users_list = filteredLst;
+    res.send();
   }
 });
